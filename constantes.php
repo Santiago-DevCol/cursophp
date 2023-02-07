83e5fbd11dbafb82c3df96a2869503e30cc6b566
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-02-07 14:04:58 -0500

    primer commit

---
+++ constantes.php
@@ -0,0 +1,13 @@
+<?php
+/*Crear un programa para convertir
+de centimetros a pulgadas y de kg a libras
+*/
+define('PULGADA',0.393701);
+define('LIBRA',2.20462);
+
+$cm=50;
+$kg=5;
+
+echo $cm * PULGADA . '<br>'; 
+echo $kg * LIBRA . '<br>';
+?>
---
+++ variable.php
@@ -0,0 +1,21 @@
+<?php
+//Propiedades de una persona
+    $nombre="Santiago";
+    $edad=23;
+    $altura=1.72;
+    $casado=true;
+    
+
+// Imprimiendo las propiedades de una persona
+    var_dump($nombre);
+    echo gettype($nombre);
+    echo '<br>';
+    var_dump($edad) ;
+    echo '<br>';
+    var_dump($altura) ;
+    echo '<br>';
+    var_dump($casado) ;
+
+
+
+?>
