44b8a8fc2b5f271201ec7c611a7bf5527e33b2d4
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-02-08 20:59:02 -0500

    Condicionales en PHP 1

---
+++ condicional_elseif.php
@@ -0,0 +1,12 @@
+<?php
+$edad=20;
+
+if ($edad>21){
+    echo "eres mayor de 21";
+}else if($edad == 21){
+    echo "Tienes 21";
+}else{
+    echo "Eres menor de 21";
+}
+
+?>
---
+++ condicional_if.php
@@ -0,0 +1,26 @@
+<?php
+/*Realizar un programa que envie un mensaje 
+solo si este contiene menos de 100 letras,
+caso contrario que arroje un mensaje de error
+ */
+
+ /*$total_letras = strlen("Hola. ¿como estas? ¿que haces?");
+
+  if ($total_letras < 100) {
+    echo "El mensaje fue enviado exitosamente";
+  }else{
+    echo "error al enviar mensaje poque contiene " . $total_letras . " letras";
+  }
+ */
+
+ /*
+ Crear un programa que indique si los habitantyes hablan español u otro idioma
+ */
+$pais="angola";
+
+if ($pais == "Mexico" || $pais == "España" || $pais == "Colombia" ){
+    echo "Aqui se habla español";
+}else{
+    echo "Aqui se habla otro idioma";
+}
+ ?>
