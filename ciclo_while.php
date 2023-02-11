26b653349e745a86dd08e4d0a8a56ad97f288c9c
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-02-10 21:32:33 -0500

    Ciclos while en PHP

---
+++ ciclo_while.php
@@ -0,0 +1,13 @@
+<?php
+    //Realizar un programa que imprima los numeros del 1 al 10
+    // las tablas de multiplicar
+    $i=1;
+    $numero = 4;
+
+    while ($i <= 10):
+       echo $numero . " X " . $i . "= " . $i ++ * $numero . '<br>';
+        
+    endwhile;
+
+
+?>
